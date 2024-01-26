a3e015703c37917b7dce25bd32fcb822d329f81b
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2024-01-26 13:17:54 +0500

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+// pages/index.js
+import React from 'react';
+
+const HomePage = ({ serverRenderedData }) => {
+  return (
+    <div>
+      <h1>Static Page with SSR</h1>
+      <p>Server-rendered data: {serverRenderedData}</p>
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  // Fetch data on the server side
+  const serverRenderedData = "Data fetched on the server side";
+  
+  return {
+    props: {
+      serverRenderedData,
+    },
+  };
+}
+
+export default HomePage;
